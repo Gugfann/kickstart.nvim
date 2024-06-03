0ff6faecb4c25fb2ae0ffb921653a9a30882fdd2
Kristian Hansen <kmha@newtec.dk>
2024-06-03 14:30:41 +0200

    Customize a couple of settings and add tmux-navigator

--- init.lua
+++ init.lua
@@ -250,8 +250,6 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  'nvim-tree/nvim-web-devicons',
-
   { 'christoomey/vim-tmux-navigator' },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
